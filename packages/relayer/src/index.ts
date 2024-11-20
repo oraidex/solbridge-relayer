803809962267d4eee82bd357db41fdfe93263973
perfogic <longdieu12x@gmail.com>
2024-11-20 18:30:27 +0700

    update all code

--- packages/relayer/src/index.ts
+++ packages/relayer/src/index.ts
@@ -3,8 +3,8 @@ import {
   StandardRelayerApp,
   StandardRelayerContext,
 } from "@wormhole-foundation/relayer-engine";
-import { CHAIN_ID_SOLANA } from "@certusone/wormhole-sdk";
-import { deserialize } from "@wormhole-foundation/sdk";
+import { CHAIN_ID_SOLANA, TokenBridgePayload } from "@certusone/wormhole-sdk";
+import { logger } from "./logger";
 
 const main = async () => {
   const app = new StandardRelayerApp<StandardRelayerContext>(
@@ -13,24 +13,82 @@ const main = async () => {
     // like retries, logger, or redis connection settings
     {
       name: "ExampleRelayer",
+      logger: logger("ExampleRelayer"),
     }
   );
 
-  app.chain(CHAIN_ID_SOLANA).address(
-    // Emitter address on Solana
-    "wormDTUJ6AWPNvk59vGQbDvGJmqbDTdgWgAqcLBCgUb",
-    // Callback function to invoke on new message
+  app.multiple(
+    {
+      [CHAIN_ID_SOLANA]: ["wormDTUJ6AWPNvk59vGQbDvGJmqbDTdgWgAqcLBCgUb"],
+    },
     async (ctx, next) => {
-      const vaaBytes = ctx.vaaBytes;
-      if (vaaBytes) {
-        const vaa = deserialize(
-          "TokenBridge:TransferWithPayload",
-          new Uint8Array(vaaBytes.buffer)
-        );
-        console.log("Got VAA", vaa);
+      let seq = ctx.vaa!.sequence.toString();
+
+      const { payload } = ctx.tokenBridge;
+      if (payload.payloadType === TokenBridgePayload.TransferWithPayload) {
+        ctx.logger.info(`chain middleware - ${seq} - ${ctx.sourceTxHash}`);
+        console.log(`Seq: ${seq}`);
+        if (seq == "1028203") {
+          console.log("FOUND ME!");
+        }
+      }
+
+      // only care about transfers
+      switch (payload.payloadType) {
+        case TokenBridgePayload.TransferWithPayload:
+          console.log("Wah ho", payload.to.toString("hex"));
+          ctx.logger.info(
+            `Transfer processing for: \n` +
+              `\tToken: ${payload.tokenChain}:${payload.tokenAddress.toString(
+                "hex"
+              )}\n` +
+              `\tAmount: ${payload.amount}\n` +
+              `\tSender ${payload.fromAddress?.toString("hex")}\n` +
+              `\tReceiver: ${payload.toChain}:${payload.to.toString("hex")}\n` +
+              `\tPayload: ${payload.tokenTransferPayload.toString("hex")}\n`
+          );
+          break;
       }
     }
   );
+
+  // app.chain(CHAIN_ID_SOLANA).address(
+  //   // Emitter address on Solana
+  //   "wormDTUJ6AWPNvk59vGQbDvGJmqbDTdgWgAqcLBCgUb",
+  //   // Callback function to invoke on new message
+  //   async (ctx, next) => {
+  //     let seq = ctx.vaa!.sequence.toString();
+  //     ctx.logger.info(`chain middleware - ${seq} - ${ctx.sourceTxHash}`);
+
+  //     const { payload } = ctx.tokenBridge;
+
+  //     // only care about transfers
+  //     switch (payload?.payloadType) {
+  //       case TokenBridgePayload.Transfer:
+  //         ctx.logger.info(
+  //           `Transfer processing for: \n` +
+  //             `\tToken: ${payload.tokenChain}:${payload.tokenAddress.toString(
+  //               "hex"
+  //             )}\n` +
+  //             `\tAmount: ${payload.amount}\n` +
+  //             `\tReceiver: ${payload.toChain}:${payload.to.toString("hex")}\n`
+  //         );
+  //         break;
+  //       case TokenBridgePayload.TransferWithPayload:
+  //         ctx.logger.info(
+  //           `Transfer processing for: \n` +
+  //             `\tToken: ${payload.tokenChain}:${payload.tokenAddress.toString(
+  //               "hex"
+  //             )}\n` +
+  //             `\tAmount: ${payload.amount}\n` +
+  //             `\tSender ${payload.fromAddress?.toString("hex")}\n` +
+  //             `\tReceiver: ${payload.toChain}:${payload.to.toString("hex")}\n` +
+  //             `\tPayload: ${payload.tokenTransferPayload.toString("hex")}\n`
+  //         );
+  //         break;
+  //     }
+  //   }
+  // );
 
   await app.listen();
 };
